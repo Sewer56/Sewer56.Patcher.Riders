9f8fb5527ab7274d14b73bdef6b79beba1d86856
Sewer 56 <seweratwork@gmail.com>
2022-01-31 04:50:18 +0000

    Added: Bootstrapper to GUI. Make CLI the only console app.

--- Patcher/Sewer56.Patcher.Regravitified/Program.cs
+++ Patcher/Sewer56.Patcher.Regravitified/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
-using Sewer56.Patcher.Riders.Cli;
-using Sewer56.Patcher.Riders.Common.Utility;
 
 namespace Sewer56.Patcher.Riders
 {
@@ -11,21 +7,9 @@ namespace Sewer56.Patcher.Riders
         [STAThread]
         public static void Main(string[] args)
         {
-            if (args.Length > 0)
-                new Commandline(args);
-            else
-                LaunchGui();
-        }
-
-        public static void LaunchGui()
-        {
-            FreeConsole();
             var app = new App();
             app.InitializeComponent();
             app.Run(new MainWindow());
         }
-
-        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-        static extern bool FreeConsole();
     }
 }
